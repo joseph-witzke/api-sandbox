6684786f957ea3c7917ed9d1820ad5d02dde8411
Justin McClellan <khalathas@gmail.com>
2022-10-05 13:49:28 -0400

    port change again

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ app.listen(port, () => {
 */
 
 // modified for heroku
-app.listen(process.env.PORT || port, () => {
+app.listen(process.env.PORT, () => {
     console.log("API is ready to rock on port " + port);
 });
 
